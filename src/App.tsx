d7af316e5dad10d62fc0383bf645d65f0566a7a1
Saketh <sakethmalapati@gmail.com>
2025-09-15 11:24:22 +0530

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ChatScreen from "./ChatScreen";
-import DropBox from "./DropBox"; 
+import DropBox from "./DropBox";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* existing chat route */}
         <Route path="/chat" element={<ChatScreen />} />
-
-        {/* new dropbox route */}
         <Route path="/dropbox" element={<DropBox />} />
       </Routes>
     </BrowserRouter>
