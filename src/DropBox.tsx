97e0d0dfbb4bf4f0488af313af96743b14b433c9
Saketh <sakethmalapati@gmail.com>
2025-09-15 11:34:00 +0530

    DropBox.jsx

--- src/DropBox.tsx
+++
@@ -1,7 +0,0 @@
-export default function DropBox() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white">
-      <h1 className="text-4xl">DropBox Component Works ✅</h1>
-    </div>
-  );
-}
