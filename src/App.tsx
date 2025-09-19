eb34da20df36acfeba0c86fdeacfda1f63c91961
Saketh <sakethmalapati@gmail.com>
2025-09-19 11:27:34 +0530

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+// src/App.tsx
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ChatScreen from "./ChatScreen";
 import DropBox from "./DropBox";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/chat" element={<ChatScreen />} />
-        <Route path="/dropbox" element={<DropBox />} />
+        <Route path="/file-uploader" element={<DropBox />} />
       </Routes>
     </BrowserRouter>
   );
