cb9918c9f173d433964366e5751157bcddd87533
Saketh <sakethmalapati@gmail.com>
2025-09-23 18:33:56 +0530

    Login and registration screens created

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ChatScreen from "./ChatScreen";
 import DropBox from "./DropBox";
 import Home from "./pages/Home";
+import Login from "./Login";
+import Register from "./Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/chat" element={<ChatScreen />} />
         <Route path="/file-uploader" element={<DropBox />} />
       </Routes>
